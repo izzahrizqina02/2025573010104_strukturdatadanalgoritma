02b935f332da4955cbedbabfe8ac6b6042daa389
izzah rizqina <izzahrizqina02@gmail.com>
2026-04-30 08:00:52 +0700

    Praktikum 1: Intro

--- praktikum-2/01-variabel.js
+++ praktikum-2/01-variabel.js
@@ -5,16 +5,16 @@
 
 //---Deklarasi dengan let (nilai bisa diubah) ---
 let nama = 'Budi';
-let umur = 20;
+const umur = 20;
 let kota = 'Banda Aceh';
 
 console.log('=== Data Mahasiswa ===');
 console.log('Nama :', nama);
-console.log('Umur:', umur);
+console.log('umur:', umur);
 console.log('Kota :',kota);
 
 // --- Mengubah nilai let ---
-umur = 21;
+//umur = 21;
 console.log('Ulang tahun! Umur sekarang:',umur);
 
 // --- Deklarasi dengan const (nilai TIDAK bisa diubah) ---
